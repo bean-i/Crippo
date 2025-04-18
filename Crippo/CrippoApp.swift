b2efbc23352d7f1a593d38ca9ccab2719598fd5a
been <leebin0603@naver.com>
2025-04-18 16:07:13 +0900

    Initial Commit

---
+++ Crippo/CrippoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrippoApp.swift
+//  Crippo
+//
+//  Created by 이빈 on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct CrippoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
